c7514be6bf3767b2d949d1272aba6b56e27531a5
iahumadaz <i.ahumada@duocuc.cl>
2025-05-17 05:39:12 -0400

    anclar la firebase

--- Proyecto/Portafolio_app/frontend/src/environments/environment.ts
+++ Proyecto/Portafolio_app/frontend/src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // URL de tu backend (Express, Cloud Function, etc.)
+  apiUrl: 'http://localhost:3000/api',
+
+  // Credenciales de Firebase (no son secretas, están en el cliente)
+  firebase: {
+    apiKey: "AIzaSyCFXl8sihWWrKRCnBi_heeQfJfrrDGOnFI",
+    authDomain: "guardar-foto-d4a53.firebaseapp.com",
+    projectId: "guardar-foto-d4a53",
+    storageBucket: "guardar-foto-d4a53.firebasestorage.app",
+    messagingSenderId: "621913189584",
+    appId: "1:621913189584:web:94ad3d03301d6e59d1c2d5",
+    measurementId: "G-678ZX5K4YC"       // opcional, sólo para analytics
+  }
 };
 
 /*
